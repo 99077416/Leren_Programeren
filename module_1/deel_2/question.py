366556719b79d4fa8e7314dc0e8e059f4fdd14f0
99077416 <99077416@mydavinci.nl>
2023-09-28 11:06:03 +0200

    commit

---
+++ module_1/deel_2/question.py
@@ -0,0 +1,6 @@
+naam = input('hallo wat is je naam? ')
+leeftijd = input(f'Goedendag {naam}, hoe oud ben je? ')
+eten = input(f'en als {leeftijd} jarige, wat eet je het liefst? ')
+drinken = input(f'en wat drink je het liefst bij {eten}? ')
+print()
+print(f'De {leeftijd} jarige {naam} drinkt het liefst {drinken} bij {eten}')
