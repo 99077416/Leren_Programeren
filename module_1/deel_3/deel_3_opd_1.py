c1010b497279b9ba2617d1572c9d2b0461b78f01
99077416 <99077416@mydavinci.nl>
2023-09-29 12:13:15 +0200

    elif-statement

--- module_1/deel_3/deel_3_opd_1.py
+++ module_1/deel_3/deel_3_opd_1.py
@@ -4,3 +4,6 @@ getal_b = int(input('voer getal in: '))
 if getal_a > getal_b: 
     max = getal_a
     print(f'a is het grootste getal: {max}')
+elif getal_a < getal_b:
+    min = getal_b
+    print(f'a is het kleinste getal: {min}')
