5523ae219c6afc751925480dc6fdead208f8cdae
99077416 <99077416@mydavinci.nl>
2023-09-29 13:11:28 +0200

    Min en Max

--- module_1/deel_3/deel_3_opd_1.py
+++ module_1/deel_3/deel_3_opd_1.py
@@ -3,10 +3,17 @@ getal_b = int(input('voer getal in: '))
 
 if getal_a > getal_b: 
     max = getal_a
+    min = getal_b
     print(f'a is het grootste getal: {max}')
 elif getal_a < getal_b:
-    min = getal_b
+    max = getal_b
+    min = getal_a
     print(f'a is het kleinste getal: {min}')
 else:
     if getal_a == getal_b:
         print('a en b zijn even groot') 
+        max = getal_a
+        min = getal_b
+
+print(f'Het minimum is: {min}')
+print(f'Het maximum is: {max}')
