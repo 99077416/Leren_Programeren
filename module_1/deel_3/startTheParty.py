52918d98175971e923c83a1ba4c648cd8c0190eb
99077416 <99077416@mydavinci.nl>
2023-09-29 13:51:42 +0200

    Party

---
+++ module_1/deel_3/startTheParty.py
@@ -0,0 +1,9 @@
+gastheer = True
+gasten = True
+drank = True
+chips = True
+
+if gastheer == True and drank == True or gasten == True and chips == True and drank == True :
+    print('Start the Party')
+else:
+    print('No Party')
