b0d655060fad62f292993fdeee208140abe055f5
99077416 <99077416@mydavinci.nl>
2023-10-12 13:57:57 +0200

    commit

--- module_1/deel_4/ice_bugs.py
+++ module_1/deel_4/ice_bugs.py
@@ -1,11 +1,12 @@
 def convertToEuroText (amount):
-    return "€{:.2f}".format(float(amount)).replace(".", ",")
+    return "€{:.2f}".format(amount).replace(".", ",")
 
 SMALL_PRICE = 1.25
 MEDIUM_PRICE = 2.10
 
 amount = int(input('Hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(SMALL_PRICE))))
+amount_small = amount * SMALL_PRICE
 amount = int(input('En hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(MEDIUM_PRICE))))
-totalPrice = (amount * SMALL_PRICE) + (amount * MEDIUM_PRICE)
-
+amount_medium = amount * MEDIUM_PRICE
+totalPrice = amount_small + amount_medium
 print('Dat is dan {} totaal'.format(convertToEuroText(totalPrice)))
