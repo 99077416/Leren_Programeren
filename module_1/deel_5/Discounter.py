1e2b4337450f6b90bc3695f98b5ffc0ecc570cb0
99077416 <99077416@mydavinci.nl>
2023-10-26 11:01:14 +0200

    Update Discounter.py

--- module_1/deel_5/Discounter.py
+++ module_1/deel_5/Discounter.py
@@ -10,3 +10,27 @@ def calc_discount(price: float, brand: str, month_discount_brands: str) -> float
 
 
 print(calc_discount(1456.54,'Vespa',month_discount_brands))
+
+
+expected = round(1456.54 * MONTH_DISCOUNT_PERC / 100,2)
+calculated = calc_discount(1456.54,'Vespa',month_discount_brands)
+test('Discount', expected, calculated)
+
+expected = round(50 * MONTH_DISCOUNT_PERC / 100,2)
+calculated = calc_discount(50,'Yamama',month_discount_brands)
+test('Discount', expected, calculated)
+
+expected = 0
+calculated = calc_discount(5000,'Volkswagen',month_discount_brands)
+test('Discount', expected, calculated)
+
+expected = round(732.96 * MONTH_DISCOUNT_PERC / 100,2)
+calculated = calc_discount(732.96,'Vespa',month_discount_brands)
+test('Discount', expected, calculated)
+
+expected = round(596 * MONTH_DISCOUNT_PERC / 100,2)
+calculated = calc_discount(596,'Kymco',month_discount_brands)
+test('Discount', expected, calculated)
+
+
+report()
