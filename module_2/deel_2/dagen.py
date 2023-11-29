94bc95c08af826f1b127bff51a46de252c159bb0
99077416 <99077416@mydavinci.nl>
2023-11-29 11:31:06 +0100

    Update dagen.py

--- module_2/deel_2/dagen.py
+++ module_2/deel_2/dagen.py
@@ -1,12 +1,25 @@
 week = ('maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag')
 
+werkdagen = week[0], week[1], week[2], week[3], week[4]
+
+weekend = week[5], week[6]
+
+week_omgekeerd = tuple(reversed(week))
+
+werkdagen_omgekeerd = tuple(reversed(werkdagen))
+
+weekend_omgekeerd = tuple(reversed(weekend))
+
 print()
-print('Alle dagen van de week zijn: ' + week[0], week[1], week[2], week[3], week[4], week[5], week[6])
+print(f'Alle dagen van de week zijn: {week}')
 print()
-print('De werkdagen zijn: ' + week[0], week[1], week[2], week[3], week[4])
+print(f'De werkdagen zijn: {werkdagen}')
 print()
-print('De weekenddagen zijn: ' + week[5], week[6])
+print(f'De weekenddagen zijn: {weekend}')
 print()
-#print('De werkdagen in omgekeerde volgorde zijn: ' + )
-week.__reversed__
-print(week)
+print(f'Alle dagen van de week in omgekeerde volgorde zijn: {week_omgekeerd}')
+print()
+print(f'De werkdagen in omgekeerde volgorde zijn: {werkdagen_omgekeerd}')
+print()
+print(f'De weekenddagen in omgekeerde volgorde zijn: {weekend_omgekeerd}')
+print()
