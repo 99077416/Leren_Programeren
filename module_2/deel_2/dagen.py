457ae50b4025fd4d75e8db76ba0b0beb6e8db1fd
99077416 <99077416@mydavinci.nl>
2023-11-29 11:20:01 +0100

    dagen

---
+++ module_2/deel_2/dagen.py
@@ -0,0 +1,12 @@
+week = ('maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag')
+
+print()
+print('Alle dagen van de week zijn: ' + week[0], week[1], week[2], week[3], week[4], week[5], week[6])
+print()
+print('De werkdagen zijn: ' + week[0], week[1], week[2], week[3], week[4])
+print()
+print('De weekenddagen zijn: ' + week[5], week[6])
+print()
+#print('De werkdagen in omgekeerde volgorde zijn: ' + )
+week.__reversed__
+print(week)
