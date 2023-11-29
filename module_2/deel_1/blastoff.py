6a24e34ace48b32f2bb57c4fd317b38d4135b24e
99077416 <99077416@mydavinci.nl>
2023-11-29 11:19:27 +0100

    blastoff

--- module_2/deel_1/blastoff.py
+++ module_2/deel_1/blastoff.py
@@ -1,9 +1,14 @@
 from time import sleep
 teller = 30
 
-for A in range(1,31):
-    sleep(0.5)
-    print(teller)
-    teller -= 1
+#for A in range(1,31):
+#    sleep(0.5)
+#    print(teller)
+#    teller -= 1
 
 print('blast off')
+
+for A in range(30,0,-1):
+    sleep(0.5)
+    print(A)
+    
