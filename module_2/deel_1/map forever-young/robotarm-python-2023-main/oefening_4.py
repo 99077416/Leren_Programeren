5f6a9385223bd9ad62507497b3ac9276320d41a8
99077416 <99077416@mydavinci.nl>
2023-11-29 11:19:39 +0100

    Update oefening_4.py

--- module_2/deel_1/map forever-young/robotarm-python-2023-main/oefening_4.py
+++ module_2/deel_1/map forever-young/robotarm-python-2023-main/oefening_4.py
@@ -4,18 +4,21 @@ robotArm = RobotArm('exercise 4')
 
 # Jouw python instructies zet je vanaf hier:
 
-for a in range(0,5):
+for a in range(0,4):
     robotArm.grab()
     robotArm.moveRight()
     robotArm.moveRight()
     robotArm.drop()
     robotArm.moveLeft()
     robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+
 
 robotArm.moveRight()
-robotArm.moveRight()
 
-for a in range(0,5):
+for a in range(0,4):
     robotArm.grab()
     robotArm.moveLeft()
     robotArm.drop()
