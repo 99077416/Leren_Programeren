80f9c6dd7e34997040751c2f8a386745ca18eff1
99077416 <99077416@mydavinci.nl>
2023-12-11 14:39:57 +0100

    Update m&m2.py

--- module_2/deel_2/m&m2.py
+++ module_2/deel_2/m&m2.py
@@ -6,12 +6,13 @@ aantal = int(input("hoeveel M&M's?: "))
 
 
 zak_mnm = {}
-
+zak_mnm['oranje'] = 0
 
 for i in range(aantal):
     kleur = choice(kleuren)
     if kleur in zak_mnm:
         zak_mnm[kleur] += 1
+        zak_mnm['oranje'] += 1
     else:
         zak_mnm[kleur] = 1
 
