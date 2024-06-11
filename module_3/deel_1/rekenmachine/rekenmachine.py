a325a20f916ac9771cead76180ba0e5a3744af54
99077416 <99077416@mydavinci.nl>
2024-06-11 10:13:37 +0200

    3110

---
+++ module_3/deel_1/rekenmachine/rekenmachine.py
@@ -0,0 +1,48 @@
+import functions
+
+choice = input('A getallen optellen \nB getallen aftrekken \nC getallen vermenigvuldigen \nD getallen delen \nE getal ophogen \nF getal verlagen \nG getal verdubbelen \nH getal halveren \nwat wilt u doen? ').lower()
+
+efgh = ['e','f','g','h']
+
+
+n1 = int(input('getal1: '))
+
+
+while choice != 'i':
+
+    n2 = int(input('getal2: '))
+
+    if choice == 'a':
+        uitkomst = functions.addition(n1,n2)
+        print(f'{n1} + {n2} = {uitkomst}')
+
+    elif choice == 'b':
+        uitkomst = functions.subtraction(n1,n2)
+        print(f'{n1} - {n2} = {uitkomst}')
+
+    elif choice == 'c':
+        uitkomst = functions.multiplication(n1,n2)
+        print(f'{n1} * {n2} = {uitkomst}')
+
+    elif choice == 'd':
+        uitkomst = functions.division(n1,n2)
+        print(f'{n1} : {n2} = {uitkomst}')
+
+    elif choice == 'e':
+        uitkomst = functions.addition(n1,1)
+        print(f'{n1} + {2} = {uitkomst}')
+
+    elif choice == 'f':
+        uitkomst = functions.subtraction(n1,1)
+        print(f'{n1} - {1} = {uitkomst}')
+
+    elif choice == 'g':
+        uitkomst = functions.multiplication(n1,2)
+        print(f'{n1} * {2} = {uitkomst}')
+
+    elif choice == 'h':
+        uitkomst = functions.division(n1,2)
+        print(f'{n1} / {2} = {uitkomst}')
+
+    n1 = uitkomst
+    choice = input(f'Wil je wat met de uitkomst ({uitkomst}) doen? A) iets optellen, B) iets aftrekken, C) met iets vermenigvuldigen, D) ergens door delen, E) ophogen, F) verlagen, G) verdubbelen, H) halveren of I) niets?').lower()
