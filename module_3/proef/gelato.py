f20628a2f88136a32e7d1e2840ba5e5da3102d70
99077416 <99077416@mydavinci.nl>
2025-02-14 11:21:35 +0100

    w

--- module_3/proef/gelato.py
+++ module_3/proef/gelato.py
@@ -5,12 +5,13 @@ bestellen = 'ja'
 
 print('Welkom bij PapiGelato.')
 
+
 while bestellen == 'ja':
     klant = int(input('Bent u 1) een particuliere klant of 2) een zakelijke klant? '))
 
     if klant == 1:
-        while True:
-
+        
+        while bestellen == 'ja':
             aantal = aantalBolletjes('Hoeveel bolletjes wilt u? ')
 
             if aantal > 8:
@@ -25,23 +26,23 @@ while bestellen == 'ja':
 
                 elif aantal >= 1 and aantal <=3:
                     verpaking = keuze_verpaking(f'Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje? ',verpaking_keuze)
-    
-            keuze_topping(aantal,verpaking,toppings,prijzen,bestelling)
-            bestelling = toevoegen(verpaking,1,bestelling)
 
-            print(f'Hier is uw {verpaking} met {aantal} bolletje(s).')
+                keuze_topping(aantal,verpaking,toppings,prijzen,bestelling)
+                bestelling = toevoegen(verpaking,1,bestelling)
 
-            bestellen = meer_bestellen('Wilt u nog meer bestellen? ',bestellen_keuze)
-            break
+                print(f'Hier is uw {verpaking} met {aantal} bolletje(s).')
+
+                bestellen = meer_bestellen('Wilt u nog meer bestellen? ',bestellen_keuze)
 
     elif klant == 2:
-        aantal = aantalBolletjes('hoeveel liter ijs wilt u? ')
-        keuze_smaak(aantal,smaken,klant,bestelling)
-        break
+
+            aantal = aantalBolletjes('hoeveel liter ijs wilt u? ')
+            keuze_smaak(aantal,smaken,klant,bestelling)
+            break
 
     else:
         print(error_text)
-    
+
 bon(bestelling,smaken,toppings,prijzen,klant)
 print()
 print('Bedankt en tot ziens!')
